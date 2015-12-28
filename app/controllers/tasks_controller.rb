09570678b837a33ca94c1b2706df4619c497d1c8
lskd <akonif@gmail.com>
2015-12-27 22:43:53 -0800

    Query adjust to display available task != current_user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,34 +1,25 @@
 class TasksController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show] # task only shows to authenticate_users
+  before_action :authenticate_user!, except: [:index] # removed :show 4 accept_task options
   before_action :set_task, only: [:show, :edit, :update, :destroy] # Task (params[:id])
   before_action :current_user_is_task_user_id, only: [:edit, :update, :destroy] # restrict non-owners
-
-
-  # Task of current_user Linked as My-TAG's in application.html.erb
-  def my_task
-    @my_id = current_user.id # may be redundant but will head towards service objects
-    @tasks = Task.where(user_id: @my_id)
-    # @tasks = Task.where(user_id: current_user.id) # works fine above is alternative
-  end
-
-  # Task of current_user which have an accepted_by_id
-  def my_task_accepted_by_another_user_id # accepted_by_id, how to acquire this?
-    @my_id = current_user.id
-    # @other_user_id = Task.where(user_id: @my_id, accepted_by_id: !empty?)
-    @tasks = Task.where(user_id: @my_id, accepted_by_id: @other_user_id) # need to figure other_user_id
-  end
-
-  # Task of accepted_task by current_user
-  def accepted_task
-    @my_id = current_user.id # redundant?
-    @task = Task.where(accepted_by_id: @my_id)
-  end
-
 
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = Task.all
+    @task_check = Task.all
+    # need to list only Task that is not current_user
+    if user_signed_in?
+      @my_id = current_user.id
+
+      # All Available task not current_user & checks for accepted_by_user_id == 0
+      # This works to show all available task not of current user that's available to accept
+      # worked only after setting accepted_by_user_id to = 0 on init via after_initialize task.rb
+      @tasks = Task.where("user_id != ? AND task_status = ? AND accepted_by_user_id = ?", @my_id, "available", 0)
+    else
+      # All Available task including my tasks : user_signed_in?(false) => index display
+      @tasks = Task.where(task_status: "available")
+      # Link clickable to accept Task logic goes here
+    end
   end
 
   # GET /tasks/1
@@ -39,6 +30,7 @@ class TasksController < ApplicationController
   # GET /tasks/new
   def new
     @task = current_user.tasks.build # Task.new
+    @my_task_status_options = {available: "available", unlist: "unlisted"}
   end
 
   # GET /tasks/1/edit
@@ -84,30 +76,55 @@ class TasksController < ApplicationController
     # else
       @task.destroy
       respond_to do |format|
-        format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
+        format.html { redirect_to tasks_url, notice: 'Task was successfully Removed.' }
         format.json { head :no_content }
       end
-    # end # this is in the private method below
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_task
-      @task = Task.find(params[:id]) # OG
-      # @task  = Task.where(current_user.id: @task.user_id) # didnt check mostly breaks I bets
-      # @tasks = Task.where(user_id: current_user.id) # This DOES break edit/delete/show
-      # @tasks = Task.where(user_id: params[:id]) # not working yet
-    end
 
-    # current_user must validate as task_user_id to Edit / Update / Delete
+
+  # Task of current_user which have an accepted_by_id
+  def my_task_accepted_by_another_user_id # accepted_by_id, != nil
+    @my_id = current_user.id
+    # @other_user_id = Task.where(user_id: @my_id, accepted_by_id: != nil) # !empty?) # preset to nil
+    @tasks = Task.where(user_id: @my_id, accepted_by_id: any?) # if !empty? doens't work != nil may
+    # @tasks = Task.where(user_id: !current_user, accepted_by_user_id: nil, task_status: "available")
+  end
+
+  # Task accepted_by_current_user of other_user_id
+  def other_user_task_accepted_by_current_user
+    @my_id = current_user.id # redundant?
+    @task = Task.where(accepted_by_id: @my_id)
+    @other_user_task_status_options = {finished: "finished", processing: "processing", error_in_tag: "errors in TAG", Return_Tag: "Please_remove_me"}
+  end
+
+  # Task of current_user Linked as My-TAG's in application.html.erb
+  def my_task
+    @my_id = current_user.id # may be redundant but will head towards service objects
+    @tasks = Task.where(user_id: @my_id)
+    # @tasks = Task.where(user_id: current_user.id) # works fine above is alternative
+    # set this in current_user_is_task_user_id private method
+    # @my_task_status_options = {available: "available", finished: "finished", complete: "complete", unavailable: "unavailable", processing: "processing"}
+  end
+
+
+  private
+  # current_user must validate as task_user_id to Edit / Update / Delete
     def current_user_is_task_user_id
+       @my_task_status_options = {available: "available", finished: "Finished '4 Review'", complete: "Complete 'n Pay'", unlist: "unlisted", processing: "processing"} # this transfers over to _form via edit call
       if current_user.id != @task.user_id
         redirect_to tasks_path
       end
     end
 
+    # Use callbacks to share common setup or constraints between actions.
+    def set_task
+      @task = Task.find(params[:id]) # OG
+      # @task  = Task.where(current_user.id: @task.user_id) # didnt check mostly breaks I bets
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
-      params.require(:task).permit(:title, :date, :price, :detail, :location)
+      params.require(:task).permit(:title, :date, :price, :detail, :location, :task_status, :accepted_by_user_id)
     end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,15 +1,28 @@
 class Task < ActiveRecord::Base
   belongs_to :user
-  after_initialize :init
+  # after_initialize do
+  #   if new_record?
+  #     :init
+  #   end
+  # end
+  # after_initialize :init, unless: :persisted? # This worked for
+  # listing but removed other unowned data from the same status fields
+   after_initialize :init, :if => :new_record? # think it failed same as above
+  # Rails 4 Way pg 279, paragraph prior to code example
+  # This was failing due to task_status not within white-listed strong params in task_controller
 
-# setting up the after_initialize default as shown in this SO answer
+
+
+
+# setting up the after_initialize default as shown in Rails 4 way pg 279 and this SO answer.
 # we want a status for all tasks and we want them to default to available on creation
 # http://stackoverflow.com/questions/328525/how-can-i-set-default-values-in-activerecord
 # doesn't overide initilize which would require the call to super in the self
 # such that it would be self.super.task_status ||= "available"
 private
    def init
-     self.task_status  ||= "available" #will set the default value only if it's nil
-     #self.address ||= build_address #let's you set a default association
+     self.accepted_by_user_id  ||= 0 #will set the default value only if it's nil
+    #  self.task_status  ||= "available" #will set the default value only if it's nil
+    #  self.address ||= build_address #let's you set a default association
    end
 end
