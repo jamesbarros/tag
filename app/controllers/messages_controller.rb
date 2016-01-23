d4f811203e8fad0971384a1e808d74a79d542f68
lskd <akonif@gmail.com>
2016-01-22 20:46:16 -0800

    mailer MessageMailer created, message_me method added and linked inside controller create method

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,9 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(message_params)
+
     if @message.valid?
+      MessageMailer.message_me(@message).deliver_now # we deliver_later for delayed_jobs
       redirect_to new_message_path, notice: "Message sent, thanks"
     else
       render :new
